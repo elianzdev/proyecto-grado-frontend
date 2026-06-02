d0dc3deea69fb07ccd3377ca130b25c9bfd81ed9
elianzdev <elianz.dev@gmail.com>
2026-06-01 21:13:26 -0500

    Estructura Frontend

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <main className="min-h-screen bg-zinc-950 text-white">
+      <section className="flex min-h-screen items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-6xl font-black">
+            Emprendix
+          </h1>
+
+          <p className="mt-4 text-zinc-400">
+            Modern Learning Platform
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default App;
